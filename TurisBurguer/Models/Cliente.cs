9a4f712de139ac72ed02dc5a1039c241f492679f
LauBena <laurabr314@gmail.com>
2024-06-05 21:02:48 -0300

    Modelo cliente creado

---
+++ TurisBurguer/Models/Cliente.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TurisBurguer.Models;
+
+public class Cliente
+{
+    [Key]
+    public int ClienteID { get; set; }
+    public string? ClienteNombre { get; set; }
+    public string? Telefono { get; set; }
+    public string? Email { get; set; }
+    public string? Direccion { get; set; }
+}
